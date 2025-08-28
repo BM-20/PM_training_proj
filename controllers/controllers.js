0b16b712d89f0f6267a70362292f7cadc64900d6
BM-20 <bellomichaelolayinka@gmail.com>
2025-08-28 09:09:06 +0100

    changes

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,4 +1,6 @@
 const transactionLog = require('../models/transactions')
+var request = require('request');
+const { Users, transactionLog } = require('../models/users');
 
 // fetch all tickers
 const getPortfolio = async (req, res) => {  
@@ -7,6 +9,25 @@ const getPortfolio = async (req, res) => {
 
 const getTicker = async (req, res) => {  
     res.send("Adding a new ticker...");
+try {
+    const { ticker } = req.params;
+    const userId = req.user.id;
+
+    const user = await Users.findByPk(userId);
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    const stock = user.stocks.find(s => s.ticker === ticker);
+    if (!stock) {
+        return res.status(404).json({ message: 'Ticker not found in portfolio' });
+    }
+
+    res.status(200).json(stock);
+} catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+}
 }
 
 const addTicker = async (req, res) => {  
@@ -54,3 +75,4 @@ module.exports = {
 
 
 
+module.exports = {getTicker}
